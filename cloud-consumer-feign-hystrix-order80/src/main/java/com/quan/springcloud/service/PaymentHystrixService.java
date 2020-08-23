0fe564a19b7c2fac860e97bfb1efd72c5c519c50
userquan <294097305@qq.com>
2020-08-24 01:51:25 +0800

    Hystrix 服务降级

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/quan/springcloud/service/PaymentHystrixService.java
@@ -0,0 +1,21 @@
+package com.quan.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @description:
+ * @author: 厚浪
+ * @create: 2020-08-20 02:37
+ **/
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT",fallback = PaymentFallbackService.class)
+public interface PaymentHystrixService {
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id);
+
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id);
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/quan/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,21 @@
+package com.quan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description:
+ * @author: 厚浪
+ * @create: 2020-08-20 01:56
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class, args);
+    }
+
+}
