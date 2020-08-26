2948d153140dd8d3355116d9070e6e6086d94b2b
userquan <294097305@qq.com>
2020-08-27 02:19:39 +0800

    GateWay globalFilter 全局自定义过滤器

--- cloud-gateway-gateway9527/src/main/java/com/quan/springcloud/fillter/MyLogGateWayFilter.java
+++ cloud-gateway-gateway9527/src/main/java/com/quan/springcloud/fillter/MyLogGateWayFilter.java
@@ -1,14 +1,49 @@
 package com.quan.springcloud.fillter;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
 
 /**
- * @description:
+ * @description: 需要实现 GlobalFilter, Ordered两个接口，
  * @author: 厚浪
  * @create: 2020-08-27 01:00
  **/
 @Component
 @Slf4j
-public class MyLogGateWayFilter {
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+    /**
+     * 全局自定义过滤器，实现自定义业务逻辑拦截
+     * http://localhost:9527/payment/lb?pwd=123&uname=111
+     * @param exchange
+     * @param chain
+     * @return
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("***********come in MyLogGateWayFilter:  "+new Date());
+
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+
+        if(uname == null)
+        {
+            log.info("*******用户名为null，非法用户，o(╥﹏╥)o");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
 }
